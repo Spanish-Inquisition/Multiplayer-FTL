e5fbdd32b801217a478f5b2ed9b59bc2692dd424
zeBaker <zkei1801@mpsedu.org>
2017-06-08 22:24:47 -0500

    new work

---
+++ cost.py
@@ -0,0 +1,77 @@
+red_ship_start_scrap = 800
+blu_ship_start_scrap = 800
+
+"""WEAPONS"""
+
+basic_laser_cost = 20
+dual_lasers_cost = 25
+burst_laser_1_cost = 50
+burst_laser_2_cost = 80
+burst_laser_3_cost = 95
+
+heavy_laser_1_cost = 55
+heavy_laser_2_cost = 65
+# lasers
+
+leto_cost = 20
+artemis_cost = 38
+hermes_cost = 45
+#missiles
+
+ion_cost = 30
+heavy_ion_cost = 45
+ion_2_cost = 70
+#ion
+
+mini_beam_cost = 20
+pike_beam_cost = 55
+halberd_beam_cost = 65
+glaive_beam_cost = 95
+#beam
+
+small_bomb_cost = 45
+ion_bomb_cost = 55
+breach_1_cost = 50
+breach_2_cost = 60
+repair_bomb = 40
+#bombs
+
+"""SYSTEMS"""
+shield_init_cost = 125
+shield_lvl_1_cost = 20
+shield_lvl_2_cost = 30
+shield_lvl_3_cost = 40
+shield_lvl_4_cost = 60
+shield_lvl_5_cost = 80
+shield_lvl_6_cost = 100
+
+engine_lvl_1_cost = 10
+engine_lvl_2_cost = 15
+engine_lvl_3_cost = 30
+engine_lvl_4_cost = 40
+engine_lvl_5_cost = 60
+engine_lvl_6_cost = 80
+engine_lvl_7_cost = 120
+
+wep_lvl_1_cost = 20
+wep_lvl_2_cost = 30
+wep_lvl_3_cost = 45
+wep_lvl_4_cost = 60
+wep_lvl_5_cost = 80
+wep_lvl_6_Cost = 100
+
+cloak_init_cost = 150
+cloak_lvl_1_cost = 30
+cloak_lvl_2_cost = 50
+cloak_lvl_3_cost = 80
+
+hack_init_cost = 85
+hack_lvl_1_cost = 35
+hack_lvl_2_cost = 60
+
+"""SUBSYSTEMS"""
+
+pilot_lvl_1_cost = 20
+pilot_lvl_2_cost = 50
+
+
